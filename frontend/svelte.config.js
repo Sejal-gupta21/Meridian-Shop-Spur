93f574e0df052b0f975c568923b079b279b50463
Sejal-gupta21 <sejalgupta21042001@gmail.com>
2026-06-15 18:52:55 +0530

    Set Vercel runtime to nodejs20.x

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: vercel({ runtime: 'nodejs18' }),
+    adapter: vercel({ runtime: 'nodejs20.x' }),
   },
 };
 
